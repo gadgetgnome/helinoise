71507072a023eb1016cb5d3e37e0f43a808decdf
Pål Smitt-Amundsen <psa@try.no>
2022-08-06 10:21:38 +0200

    Plane in place with fog and controls

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,14 @@
 import * as THREE from "three";
+import { FlyControls } from "three/examples/jsm/controls/FlyControls.js";
 
+// Scene
 const scene = new THREE.Scene();
+scene.fog = new THREE.Fog(0x0, 0, 1);
+
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
-  0.1,
+  0.01,
   1000
 );
 
@@ -12,25 +16,39 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const planeGeo = new THREE.PlaneBufferGeometry(1, 1, 100);
-const geoMaterial = new THREE.LineBasicMaterial({
+const geometry = new THREE.PlaneGeometry(100, 100, 1600, 1600);
+const material = new THREE.MeshBasicMaterial({
   color: 0xffffff,
-  linewidth: 1,
-  linecap: "round", //ignored by WebGLRenderer
-  linejoin: "round", //ignored by WebGLRenderer
 });
-const planeMesh = new THREE.Mesh(planeGeo, geoMaterial);
+const cube = new THREE.Mesh(geometry, material);
+material.wireframe = true;
 
-scene.add(planeMesh);
+window.material = material;
 
-camera.position.x = camera.position.z = camera.position.y = 10;
-camera.lookAt(planeMesh);
+scene.add(cube);
 
-const render = () => {
+camera.position.y = 0.1;
+// camera.rotation.x = -Math.PI / 4;
+
+const controls = new FlyControls(camera, renderer.domElement);
+
+controls.movementSpeed = 0.5;
+controls.domElement = renderer.domElement;
+controls.rollSpeed = Math.PI / 24;
+controls.autoForward = true;
+controls.dragToLook = false;
+
+const clock = new THREE.Clock();
+
+function animate() {
+  const delta = clock.getDelta();
+  requestAnimationFrame(animate);
+
+  cube.rotation.x = Math.PI / 2;
+  // cube.rotation.y += 0.01;
+
   renderer.render(scene, camera);
-  requestAnimationFrame(render);
-};
+  controls.update(delta);
+}
 
-render();
-
-console.log("OK");
+animate();
