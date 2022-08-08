6430bfc2a1b3a93075dfbab13991091791c912bf
Pål Smitt-Amundsen <psa@try.no>
2022-08-08 13:40:27 +0200

    Add lights, but disabled due to unwanted effects

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
-import Noise from "noisejs";
+import { Noise } from "noisejs";
 import { FlyControls } from "three/examples/jsm/controls/FlyControls.js";
 
 // Scene
 const scene = new THREE.Scene();
-scene.fog = new THREE.Fog(0x0, 0, 1);
+scene.fog = new THREE.Fog(0x0, 0, 4);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -17,17 +17,31 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// GEO
+// GEO // TERRAIN
 const geometry = new THREE.PlaneGeometry(100, 100, 1600, 1600);
 
-geometry.attributes.position.array = geometry.attributes.position.array.map(
-  (p, i) => ((i + 1) % 3 === 0 ? p + Math.random() * 0.1 : p)
-);
+// https://www.npmjs.com/package/noisejs
+
+const geoArr = geometry.attributes.position.array;
+console.time("terrain");
+const noise = new Noise(0);
+const noiseFrequency = 0.2;
+const noiseAmp = 1;
+for (let index = 2; index < geoArr.length; index += 3) {
+  geoArr[index] =
+    noise.simplex2(
+      geoArr[index - 2] * noiseFrequency,
+      geoArr[index - 1] * noiseFrequency
+    ) * noiseAmp;
+}
+console.timeEnd("terrain");
+
+geometry.computeBoundingBox();
+geometry.computeBoundingSphere();
 geometry.computeVertexNormals();
 geometry.computeTangents();
 
-// MAt
-
+// Material
 const material = new THREE.MeshBasicMaterial({
   color: 0xffffff,
 });
@@ -38,28 +52,43 @@ window.material = material;
 
 scene.add(cube);
 
-camera.position.y = 0.1;
+const heli = new THREE.Object3D();
+heli.add(camera);
+heli.position.y = 0.2;
+scene.add(heli);
 // camera.rotation.x = -Math.PI / 4;
 
-const controls = new FlyControls(camera, renderer.domElement);
+// Lights
+// const light = new THREE.PointLight(0xffffff, 1, 1);
+// heli.add(light);
+// window.light = light;
 
-controls.movementSpeed = 0.5;
-controls.domElement = renderer.domElement;
-controls.rollSpeed = Math.PI / 24;
-controls.autoForward = true;
-controls.dragToLook = false;
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+// scene.add(ambientLight);
+
+// Controls
+
+// const controls = new FlyControls(camera, renderer.domElement);
+
+// controls.movementSpeed = 0.5;
+// controls.domElement = renderer.domElement;
+// controls.rollSpeed = Math.PI / 24;
+// controls.autoForward = true;
+// controls.dragToLook = false;
 
 const clock = new THREE.Clock();
+cube.rotation.x = -Math.PI / 2;
 
 function animate() {
   const delta = clock.getDelta();
   requestAnimationFrame(animate);
 
-  cube.rotation.x = Math.PI / 2;
   // cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
-  controls.update(delta);
+  heli.position.z -= 0.002;
+  heli.rotation.z = Math.sin(new Date() / 1000) / 4;
+  // controls.update(delta);
 }
 
 animate();
